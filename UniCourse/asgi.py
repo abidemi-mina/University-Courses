972b9101bc51dd1ed91ecf0d9875307b309a6e3f
abidemi-mina <aminatabidemi212@gmail.com>
2023-05-10 16:22:32 +0100

    'first-commit'

---
+++ UniCourse/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for UniCourse project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UniCourse.settings')
+
+application = get_asgi_application()
